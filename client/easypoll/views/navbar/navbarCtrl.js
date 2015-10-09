a9770954f30778a198aea61b3459333ca766ba18
jpiquet <jpiquet@xiti.com>
2015-10-09 13:35:58 +0200

    fin navbar custom

--- client/easypoll/views/navbar/navbarCtrl.js
+++ client/easypoll/views/navbar/navbarCtrl.js
@@ -1,15 +1,55 @@
 angular.module('easypoll')
 
     .controller('navbarCtrl',['$scope','$state',function($scope,$state){
-        //=== Variables ===//
+
 
         $scope.brand = "<span class='glyphicon glyphicon-question-sign'></span> EasyPoll";
 
-        $scope.menus = []; // end menus
+        $scope.buttons = [];
 
-        $scope.searchfn = function(){
-            alert('Attempting search on: "' + $scope.search.terms + '"');
-        }; // searchfn
+
+        $scope.navfn = function(action){
+            switch(action){
+                case 'signOut':
+                    Meteor.logout(function(err) {
+                        if(!err) {
+                            $scope.$emit('logout');
+                            $state.go("login");
+                        }
+                    });
+                    break;
+                case 'signIn':
+                    $state.go("login");
+                    break;
+                default:
+                    break;
+            };
+        };
+
+        $scope.$on('logout', function (event) {
+            $scope.buttons.length=0;
+            $scope.buttons.push(
+                {
+                    title:"Sign-in",
+                    action:"signIn",
+                    spanClass:"glyphicon glyphicon-log-in",
+                    buttonClass:"btn btn-primary navbar-button"
+                }
+            );
+        });
+
+        $scope.$on('login', function (event) {
+            $scope.buttons.length=0;
+            $scope.buttons.push(
+                {
+                    title:"Sign-out",
+                    action:"signOut",
+                    spanClass:"glyphicon glyphicon-log-out",
+                    buttonClass:" btn btn-default navbar-button"
+                }
+            );
+        });
+
 
         if(Meteor.userId()){
             $scope.$emit('login');
