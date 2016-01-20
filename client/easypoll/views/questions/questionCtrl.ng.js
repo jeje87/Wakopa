444bc3ce0a47ac913a9af839de4cdf85c1f40cff
jeje87 <jeromepiquet@free.fr>
2016-01-20 14:17:09 +0100

    reformat code

--- client/easypoll/views/questions/questionCtrl.ng.js
+++ client/easypoll/views/questions/questionCtrl.ng.js
@@ -1,174 +1,172 @@
 angular.module("easypoll").controller("QuestionCtrl",
-    function ($scope, $stateParams,$meteor,$location,Notification,meteorService) {
+    function ($scope, $stateParams, $meteor, $location, Notification, meteorService) {
 
-    //*********************************************************************************************
-    //************************************ Déclarations *******************************************
-    //*********************************************************************************************
+        //*********************************************************************************************
+        //************************************ Déclarations *******************************************
+        //*********************************************************************************************
 
-    //$scope.allowSave=false;
-    //$scope.pageClass = 'page-home';
-    $scope.results = {};
-    $scope.navSelectedName="";
+        //$scope.allowSave=false;
+        //$scope.pageClass = 'page-home';
+        $scope.results = {};
+        $scope.navSelectedName = "";
 
-    $scope.subscribe('Questions', () => [
-    ]);
+        $scope.subscribe('Questions', () => []);
 
-    $scope.subscribe('Templates', () => [
-    ]);
+        $scope.subscribe('Templates', () => []);
 
-    //*********************************************************************************************
-    //********************************* Méthodes privées ******************************************
-    //*********************************************************************************************
+        //*********************************************************************************************
+        //********************************* Méthodes privées ******************************************
+        //*********************************************************************************************
 
         let manageError = (error) => {
             Notification.Error('An error has occurred');
             console.log(error);
         };
 
-    let sendMail = (idRespondent) => {
+        let sendMail = (idRespondent) => {
 
-        meteorService.sendQuestion($scope.question, idRespondent)
-            .then(()  => {
-                Notification.success('Question sended successfully');
-            })
-            .catch(err => {
-                manageError(err)
-            });
-    };
-
-    //renvoi les résultats de la question affichée
-    let getResultsClient = function (questionId) {
-
-        meteorService.getResults(questionId)
-            .then((data)  => {
-                $scope.safeApply(function () {
-                    $scope.results.labels = data.labels;
-                    $scope.results.data = data.values;
+            meteorService.sendQuestion($scope.question, idRespondent)
+                .then(() => {
+                    Notification.success('Question sended successfully');
+                })
+                .catch(err => {
+                    manageError(err)
                 });
-            })
-            .catch(err => {
-                manageError(err)
-            });
+        };
 
-    };
+        //renvoi les résultats de la question affichée
+        let getResultsClient = function (questionId) {
 
-    let init = function () {
+            meteorService.getResults(questionId)
+                .then((data) => {
+                    $scope.safeApply(function () {
+                        $scope.results.labels = data.labels;
+                        $scope.results.data = data.values;
+                    });
+                })
+                .catch(err => {
+                    manageError(err)
+                });
 
-        if($stateParams.id && $stateParams.id!=="new") {
+        };
+
+        let init = function () {
+
+            if ($stateParams.id && $stateParams.id !== "new") {
+
+                $scope.helpers({
+                    question: () => {
+                        return Questions.findOne($stateParams.id);
+                    }
+                });
+
+            }
+            else {
+
+                $scope.helpers({
+                    question: () => {
+                        return Templates.findOne({"name": "question"});
+                    }
+                });
+
+                $scope.setActiveTab("Answers");
+            }
 
             $scope.helpers({
-                question: () => {
-                    return Questions.findOne($stateParams.id);
+                templateAnswers: () => {
+                    return Templates.findOne({"name": "templateAnswers"});
                 }
             });
 
-        }
-        else {
+        };
 
-            $scope.helpers({
-                question: () => {
-                    return Templates.findOne({"name":"question"});
-                }
-            });
+        //*********************************************************************************************
+        //********************************* Méthodes publiques ****************************************
+        //*********************************************************************************************
 
-            $scope.setActiveTab("Answers");
-        }
+        /* conservation tab active*/
+        $scope.setActiveTab = function (activeTab, elem) {
+            localStorage.setItem("activeTab", activeTab);
+        };
 
-        $scope.helpers({
-            templateAnswers: () => {
-                return Templates.findOne({"name":"templateAnswers"});
+        $scope.getActiveTab = function () {
+            return localStorage.getItem("activeTab");
+        };
+
+        $scope.isActiveTab = function (index) {
+            var activeTab = $scope.getActiveTab();
+            return (activeTab === index );
+        };
+        /* conservation tab active*/
+
+        $scope.save = function () {
+
+            meteorService.saveQuestion($scope.question)
+                .then((id) => {
+                    if ($stateParams.id && $stateParams.id === "new") {
+                        Notification.success('Question added successfully');
+                        $location.path("/question/" + id);
+                    }
+                    else {
+                        Notification.success('Changes saved successfully');
+                    }
+                })
+                .catch(err => {
+                    manageError(err)
+                });
+
+        };
+
+        $scope.delete = function () {
+            $scope.question.deleteDate = new Date();
+            $scope.save();
+            $scope.back();
+        };
+
+        $scope.back = function () {
+            $location.path("/questionList");
+        };
+
+        $scope.dpdTypeSelect = function (item) {
+            item._id = Random.id();
+            $scope.question.answers = item.values;
+        };
+
+        $scope.addAnswer = function (label) {
+            $scope.question.answers.push({"_id": Random.id(), "label": label});
+        };
+
+        $scope.removeAnswer = function (id) {
+            $scope.question.answers = _.without($scope.question.answers, _.findWhere($scope.question.answers, {_id: id}));
+        };
+
+        $scope.addRespondent = function (label) {
+            $scope.question.respondents.push({"_id": Random.id(), "label": label});
+        };
+
+        $scope.removeRespondent = function (id) {
+            $scope.question.respondents = _.without($scope.question.respondents, _.findWhere($scope.question.respondents, {_id: id}));
+        };
+
+        $scope.sendAll = function () {
+            sendMail();
+        };
+
+        $scope.sendTo = function (idRespondent) {
+            sendMail(idRespondent);
+        };
+
+        $scope.autorun(() => {
+            if ($stateParams.id && $stateParams.id !== "new") {
+                getResultsClient($stateParams.id);
             }
         });
 
-    };
+        //*********************************************************************************************
+        //********************************** Initialisation *******************************************
+        //*********************************************************************************************
 
-    //*********************************************************************************************
-    //********************************* Méthodes publiques ****************************************
-    //*********************************************************************************************
-
-    /* conservation tab active*/
-    $scope.setActiveTab = function( activeTab, elem ){
-        localStorage.setItem("activeTab", activeTab);
-     };
-
-    $scope.getActiveTab = function(){
-        return localStorage.getItem("activeTab");
-    };
-
-    $scope.isActiveTab = function( index ){
-        var activeTab = $scope.getActiveTab();
-        return (activeTab === index );
-    };
-    /* conservation tab active*/
-
-    $scope.save = function() {
-
-        meteorService.saveQuestion($scope.question)
-            .then((id)  => {
-                if($stateParams.id && $stateParams.id==="new") {
-                    Notification.success('Question added successfully');
-                    $location.path("/question/" + id);
-                }
-                else {
-                    Notification.success('Changes saved successfully');
-                }
-            })
-            .catch(err => {
-                manageError(err)
-            });
-
-    };
-
-    $scope.delete = function() {
-        $scope.question.deleteDate=new Date();
-        $scope.save();
-        $scope.back();
-    };
-
-    $scope.back = function() {
-        $location.path("/questionList");
-    };
-
-    $scope.dpdTypeSelect = function(item) {
-        item._id = Random.id();
-        $scope.question.answers=item.values;
-    };
-
-    $scope.addAnswer = function(label) {
-        $scope.question.answers.push({"_id":Random.id(),"label":label});
-    };
-
-    $scope.removeAnswer = function(id) {
-        $scope.question.answers = _.without($scope.question.answers, _.findWhere($scope.question.answers, {_id: id}));
-    };
-
-    $scope.addRespondent = function(label) {
-        $scope.question.respondents.push({"_id":Random.id(),"label":label});
-    };
-
-    $scope.removeRespondent = function(id) {
-        $scope.question.respondents = _.without($scope.question.respondents, _.findWhere($scope.question.respondents, {_id: id}));
-    };
-
-    $scope.sendAll = function() {
-        sendMail();
-    };
-
-    $scope.sendTo = function(idRespondent) {
-        sendMail(idRespondent);
-    };
-
-    $scope.autorun(() => {
-        if($stateParams.id && $stateParams.id!=="new") {
-            getResultsClient($stateParams.id);
-        }
-    });
-
-    //*********************************************************************************************
-    //********************************** Initialisation *******************************************
-    //*********************************************************************************************
-
-    init();
+        init();
 
     }
 );
