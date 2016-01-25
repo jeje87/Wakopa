2a4ba7db91f8f7888e3e0ea58613311f7f34b6ba
jeje87 <jeromepiquet@free.fr>
2016-01-25 21:56:51 +0100

    ajout template answer

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,31 +1,79 @@
 // run this when the meteor app is started
-Meteor.startup(function() {
+Meteor.startup(function () {
 
     // if there are no polls available create sample data
-    if (Templates.find({"name":"question"}).count() === 0) {
+    if (Templates.find({"name": "question"}).count() === 0) {
 
         // create sample polls
         var question =
-            {
-                "_id" : "-1",
-                "name" : "question",
-                "label" : "",
-                "answers" : [
-                    {
-                        "_id" : "1",
-                        "label" : ""
-                    },
-                    {
-                        "_id" : "2",
-                        "label" : ""
-                    }
-                ],
-                "respondents" : [],
-                "createDate" : "",
-                "userId" : "",
-                "mails" : []
-            }
-        ;
+        {
+            "_id": "-1",
+            "name": "question",
+            "label": "",
+            "answers": [
+                {
+                    "_id": "1",
+                    "label": ""
+                },
+                {
+                    "_id": "2",
+                    "label": ""
+                }
+            ],
+            "respondents": [],
+            "createDate": "",
+            "userId": "",
+            "mails": []
+        };
+
         Templates.insert(question);
     }
+
+    if (Templates.find({"name": "templateAnswers"}).count() === 0) {
+
+        // create sample polls
+        var templateAnswers =
+        {
+            "_id": "-2",
+            "name": "templateAnswers",
+            "templates": [
+                {
+                    "_id": "1",
+                    "label": "Yes or No",
+                    "values": [
+                        {
+                            "_id": 1,
+                            "label": "Yes"
+                        },
+                        {
+                            "_id": 2,
+                            "label": "No"
+                        }
+                    ]
+                },
+                {
+                    "_id": "2",
+                    "label": "Yes, No or Maybe",
+                    "values": [
+                        {
+                            "_id": 1,
+                            "label": "Yes"
+                        },
+                        {
+                            "_id": 2,
+                            "label": "No"
+                        },
+                        {
+                            "_id": 3,
+                            "label": "Maybe"
+                        }
+                    ]
+                }
+            ]
+        };
+
+        Templates.insert(templateAnswers);
+    }
+
+
 });
