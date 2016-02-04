d6462a0be3907584e764f3398caf845232ef0e8b
jeje87 <jeromepiquet@free.fr>
2016-02-04 14:15:59 +0100

    récupération réponses basées sur l'email

--- server/publish.js
+++ server/publish.js
@@ -13,9 +13,23 @@ Meteor.publish('QuestionsView', function () {
 //https://atmospherejs.com/percolate/find-from-publication
 //https://www.discovermeteor.com/blog/pagination-problems-meteor/
 FindFromPublication.publish('AnswersUser', function() {
+
+    var user = Meteor.users.findOne(this.userId);
+    var mail="-1";
+
+    if(user.services && user.services.google ) {
+        mail = user.services.google.email;
+    }
+    else if(user.services && user.services.facebook) {
+        mail = user.services.facebook.email;
+    }
+    else if(user.emails) {
+        mail = user.emails[0].address;
+    }
+
     return Questions.find(
         {
-            "respondents._id": this.userId,
+            "respondents.email": mail,
             "deleteDate": {$exists: false}
         },
         {
