da8164fe9ad57d226ce923f519fee7f420231e36
jeje87 <jeromepiquet@free.fr>
2016-03-05 11:05:37 +0100

    gestion barre navigation OK

--- client/easypoll/views/navbar/navbarCtrl.js
+++ client/easypoll/views/navbar/navbarCtrl.js
@@ -13,7 +13,6 @@ angular.module('easypoll')
                     case 'signOut':
                         Meteor.logout(function (err) {
                             if (!err) {
-                                $scope.$emit('logout');
                                 $state.go("login");
                             }
                         });
@@ -22,60 +21,62 @@ angular.module('easypoll')
                         $state.go("login");
                         break;
                     case 'back':
-                        $scope.$emit('questionList');
                         $state.go("questionList");
                         break;
                     default:
                         break;
                 }
-                ;
             };
 
-            $scope.$on('$locationChangeStart', function(event) {
-//TODO
-            });
-
-            $scope.$on('logout', function (event) {
-                $scope.buttons.length = 0;
-                $scope.buttons.push(
-                    {
-                        title: "Sign-in",
-                        action: "signIn",
-                        spanClass: "glyphicon glyphicon-log-in",
-                        buttonClass: "btn btn-primary navbar-button"
+            $scope.$on('$locationChangeSuccess', function(event, next, current) {
+                if(Meteor.user()) {
+                    $scope.buttons.length = 0;
+                    $scope.leftbuttons.length = 0;
+                    //Meteor.user().profile.name.split(" ")[0]
+                    $scope.buttons.push(
+                        {
+                            title: "Sign-out",
+                            action: "signOut",
+                            spanClass: "glyphicon glyphicon-log-out",
+                            buttonClass: " btn btn-default navbar-button"
+                        }
+                    );
+                    if(next.endsWith("questions")) {
+                        $scope.leftbuttons.push(
+                            {
+                                title: "",
+                                action: "back",
+                                spanClass: "glyphicon glyphicon-arrow-left",
+                                buttonClass: " btn btn-default navbar-button disabled"
+                            }
+                        );
                     }
-                );
-                $scope.username = "";
-            });
-
-            $scope.$on('login', function (event) {
-                $scope.buttons.length = 0;
-                $scope.buttons.push(
-                    {
-                        title: Meteor.user().profile.name.split(" ")[0],
-                        action: "signOut",
-                        spanClass: "glyphicon glyphicon-log-out",
-                        buttonClass: " btn btn-default navbar-button"
+                    else {
+                        $scope.leftbuttons.push(
+                            {
+                                title: "",
+                                action: "back",
+                                spanClass: "glyphicon glyphicon-arrow-left",
+                                buttonClass: " btn btn-default navbar-button"
+                            }
+                        );
                     }
-                );
-                $scope.leftbuttons.push(
-                    {
-                        title: "",
-                        action: "back",
-                        spanClass: "glyphicon glyphicon-arrow-left",
-                        buttonClass: " btn btn-default navbar-button"
+                }
+                else {
+                    $scope.buttons.length = 0;
+                    $scope.leftbuttons.length = 0;
+                    if(next.endsWith("login")===-1) {
+                        $scope.buttons.push(
+                            {
+                                title: "Sign-in",
+                                action: "signIn",
+                                spanClass: "glyphicon glyphicon-log-in",
+                                buttonClass: "btn btn-primary navbar-button"
+                            }
+                        );
                     }
-                );
-
+                }
             });
-
-
-            if (Meteor.userId()) {
-                $scope.$emit('login');
-            }
-            else {
-                $scope.$emit('logout');
-            }
 
         }
     );
