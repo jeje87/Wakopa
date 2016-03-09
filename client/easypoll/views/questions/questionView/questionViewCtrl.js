e927433b3c25779a5aca535ce5af9d70af00b263
jeje87 <jeromepiquet@free.fr>
2016-03-09 23:14:20 +0100

    utilisation this.autorun

--- client/easypoll/views/questions/questionView/questionViewCtrl.js
+++ client/easypoll/views/questions/questionView/questionViewCtrl.js
@@ -1,119 +1,111 @@
 angular.module("easypoll").controller("QuestionViewCtrl",
-    function ($scope, $stateParams, $meteor, $location, Notification, meteorService, $reactive) {
+    function ($scope, $stateParams,$meteor,$location,Notification,meteorService,$reactive) {
 
-        //*********************************************************************************************
-        //************************************ Déclarations *******************************************
-        //*********************************************************************************************
+    //*********************************************************************************************
+    //************************************ Déclarations *******************************************
+    //*********************************************************************************************
 
-        $reactive(this).attach($scope);
-        var self=this;
+    $scope.results = {};
+    $scope.user = {};
+    $scope.totValue = 0;
+    var self=this;
+    $reactive(self).attach($scope);
 
-        $scope.results = {};
-        $scope.user = {};
-        $scope.totValue = 0;
+    //*********************************************************************************************
+    //********************************* Méthodes privées ******************************************
+    //*********************************************************************************************
 
-        //*********************************************************************************************
-        //********************************* Méthodes privées ******************************************
-        //*********************************************************************************************
+    //renvoi la réponse de l'utilisateur connecté
+    let getAnswerUser = function () {
 
-        //renvoi la réponse de l'utilisateur connecté
-        let getAnswerUser = function () {
+        self.call('getAnswerUser', $stateParams.questionId, $stateParams.respondentId, $stateParams.answerId, function(err,data) {
 
-            self.call('getAnswerUser', $stateParams.questionId, $stateParams.respondentId, $stateParams.answerId, function (err, data) {
-
-                if (err) {
-                    Notification.error('An error has occurred (getAnswerUser)');
-                    console.log(err);
-                    return;
-                }
-
-                if (data) {
-
-                    $scope.user.to = data.email;
-                    $scope.user.answers = data.answers;
-                }
-
-            });
-        };
-
-        //renvoi les résultats de la question affichée
-        let getResultsClient = function () {
-
-            self.call('getResults', $stateParams.questionId, function (err, data) {
-
-                if (err) {
-                    Notification.Error('An error has occurred');
-                    console.log(err);
-                    return;
-                }
-
-                $scope.totValue = data.values.reduce(function (prev, cur) {
-                    return prev + cur;
-                });
-
-                //$scope.safeApply(function(){
-                    $scope.results.labels = data.labels;
-                    $scope.results.data = data.values;
-                //});
-
-
-            });
-        };
-
-        let init = function () {
-            getAnswerUser();
-        };
-
-        //*********************************************************************************************
-        //********************************* Méthodes publiques ****************************************
-        //*********************************************************************************************
-
-        //action du vote
-        $scope.vote = function (answer) {
-
-            $scope.user.answers.length = 0;
-            $scope.user.answers.push(answer._id);
-
-            self.call('selectAnswer', $stateParams.questionId, answer._id, $stateParams.respondentId, $stateParams.answerId, function (err, data) {
-
-                if (err) {
-                    Notification.error('An error has occurred');
-                    console.log(err);
-                    return;
-                }
-
-                //Notification.success('Changes saved successfully');
-
-            });
-        };
-
-        $scope.selectedByMe = function (answer) {
-            return _.contains($scope.user.answers, answer._id);
-        };
-
-        this.autorun(function () {
-            meteorService.subscribe("QuestionsView");
-            getResultsClient();
-        });
-
-        //lie la variable $scope.question à la collection Mongo
-        $scope.helpers({
-            question: () => {
-                return Questions.findOne($stateParams.questionId);
+            if(err){
+                Notification.error('An error has occurred (getAnswerUser)');
+                console.log(err);
+                return;
             }
+
+            if(data) {
+
+                $scope.user.to = data.email;
+                $scope.user.answers = data.answers;
+            }
+
         });
+    };
 
-        $scope.back = () => {
-            $location.path("/questionList");
-        };
+    //renvoi les résultats de la question affichée
+    let getResultsClient = function () {
 
-        //*********************************************************************************************
-        //********************************** Initialisation *******************************************
-        //*********************************************************************************************
+        self.call('getResults', $stateParams.questionId  ,function(err,data) {
 
-        init();
+            if(err){
+                Notification.error('An error has occurred');
+                console.log(err);
+                return;
+            }
 
-        //http://localhost:3000/question/view/qXmKcfyHNWpy3ZwD5/sZ2zmuKpLxwxw7vA3/Zt5PMDMGhReZ5zvBb#tabAllAnswers
+            $scope.totValue = data.values.reduce(function(prev, cur) {
+                return prev + cur;
+            });
+            Notification.info('ok');
+      //      $scope.safeApply(function () {
+                $scope.results.labels = data.labels;
+                $scope.results.data = data.values;
+       //     });
+
+        });
+    };
+
+    let init = function () {
+        getAnswerUser();
+    };
+
+    //*********************************************************************************************
+    //********************************* Méthodes publiques ****************************************
+    //*********************************************************************************************
+
+    //action du vote
+    $scope.vote = function(answer) {
+
+        $scope.user.answers.length = 0;
+        $scope.user.answers.push(answer._id);
+
+        self.call('selectAnswer', $stateParams.questionId, answer._id, $stateParams.respondentId, $stateParams.answerId ,function(err,data) {
+
+            if(err){
+                Notification.error('An error has occurred');
+                console.log(err);
+                return;
+            }
+
+            //Notification.success('Changes saved successfully');
+
+        });
+    };
+
+    $scope.selectedByMe = function(answer) {
+        return _.contains($scope.user.answers, answer._id);
+    };
+
+    this.autorun(() => {
+        meteorService.subscribe("QuestionsView");
+        self.question = Questions.findOne($stateParams.questionId);
+        getResultsClient();
+    });
+
+    $scope.back = () => {
+        $location.path("/questionList");
+    };
+
+    //*********************************************************************************************
+    //********************************** Initialisation *******************************************
+    //*********************************************************************************************
+
+    init();
+
+    //http://localhost:3000/question/view/qXmKcfyHNWpy3ZwD5/sZ2zmuKpLxwxw7vA3/Zt5PMDMGhReZ5zvBb#tabAllAnswers
 
     }
 );
